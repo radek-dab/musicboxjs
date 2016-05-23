06406635f6db6d661a2e4ff503f72f85e20e0b8c
Radosław Dąbrowski <radek.dab@gmail.com>
2016-05-23 20:01:37 +0200

    Check NODE_ENV for Raspberry Pi environment

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var morgan = require('morgan');
 var Player = require('player');
 var config = require('./config');
-var rpi = require('./rpi');
 
 var app = express();
 app.use(morgan(config.logFormat));
@@ -18,7 +17,6 @@ player = new Player([
   __dirname + '/music/lazarev.mp3',
   __dirname + '/music/szpak.mp3'
 ]);
-rpi(player);
 
 player.on('playing', function(song) {
   console.log(song);
@@ -28,3 +26,8 @@ player.on('playing', function(song) {
 });
 
 player.play();
+
+if (process.env.NODE_ENV == 'raspberrypi') {
+  console.log('Raspberry Pi environment');
+  require('./rpi')(player);
+}
