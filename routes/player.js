a59de39f73e7488f3d06d1286ed083cb15afaf30
mcibor <ciborowskim@outlook.com>
2016-06-01 15:50:08 +0200

    Add sendMpdStatus function

--- routes/player.js
+++ routes/player.js
@@ -1,6 +1,13 @@
 var router = require('express').Router();
 var player = require('../player');
 
+var sendMpdStatus = function (next, res) {
+  player.status(function (err, status) {
+    if (err) return next(err);
+    res.json(status);
+  });  
+};
+
 router.get('/library', function (req, res, next) {
   player.getLibrary(function (err, tracks) {
     if (err) return next(err);
@@ -10,20 +17,14 @@ router.get('/library', function (req, res, next) {
 
 
 router.get('/status', function (req, res, next) {
-  player.status(function (err, status){
-    if (err) return next(err);
-    res.json(status);
-  })
+  sendMpdStatus(next, res);
 })
 
 //Use play if mpd status is "stop" - returns mpd status
 router.get('/play', function (req, res, next) {
   player.play(function (err, msg) {
     if (err) return next(err);
-    player.status(function (err, msg) {
-      if (err) return next(err);
-      res.json(msg);
-    });
+    sendMpdStatus(next, res);
   });
 });
 
@@ -35,30 +36,21 @@ router.param('arg', function (req, res, next, id) {
 router.get('/pause/:arg', function (req, res, next) {
   player.pause(req.params.arg, function (err){
     if (err) return next(err);
-    player.status(function (err, msg) {
-      if (err) return next(err);
-      res.json(msg);
-    });
+    sendMpdStatus(next, res);
   })
 });
 
 router.get('/next', function (req, res, next) {
   player.nextSong(function (err) {
     if (err) return next(err);
-    player.status(function (err, msg) {
-      if (err) return next(err);
-      res.json(msg);
-    });
+    sendMpdStatus(next, res);
   });
 });
 
 router.get('/prev', function (req, res, next) {
   player.previousSong(function (err) {
     if (err) return next(err);
-    player.status(function (err, msg) {
-      if (err) return next(err);
-      res.json(msg);
-    });
+    sendMpdStatus(next, res);
   });
 });
 
@@ -66,10 +58,7 @@ router.get('/prev', function (req, res, next) {
 router.get('/plclear', function (req, res, next) {
   player.clearPlaylist(function (err) {
     if (err) return next(err);
-    player.status(function (err, msg) {
-      if (err) return next(err);
-      res.json(msg);
-    });
+    sendMpdStatus(next, res);
   });
 });
 
@@ -91,7 +80,5 @@ router.get('/pladdall', function (req, res, next) {
     res.json(songs)
   });
 });
-
-
 
 module.exports = router;
